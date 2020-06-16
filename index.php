9ee0c8acb81d24bfbc4258304e883789f28aac83
Shailaja Tripathi <58202024+radhika345@users.noreply.github.com>
2020-06-16 18:40:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,1 @@
 <?php include_once("home.html"); ?>
-<?php include_once("event.html"); ?>
-<?php include_once("about.html"); ?>
-<?php include_once("contact.html"); ?>
-<?php include_once("faq.html"); ?>
